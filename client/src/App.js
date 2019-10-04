edba770b911e1ea9dc9ae119f3083e3367a00217
abarne <celius377@gmail.com>
2019-10-04 11:43:35 -0500

    data is returned and dislpayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			people: [],
+			test: 'hi'
+		};
+	}
+
+	componentDidMount() {
+		axios.get(`http://localhost:5000/api/players`).then((response) => {
+			this.setState({ people: response.data });
+			console.log(this.state.people);
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.people.map((item) => (
+					<div className="person">
+						<h1>Name: {item.name}</h1>
+						<h2>Country: {item.country}</h2>
+						<h2>Searches: {item.searches}</h2>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
